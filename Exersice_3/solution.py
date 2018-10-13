d8e05bacac94e30d4d6cf4a96cde79c7af867142
Eli <alexeykhitrov@gmail.com>
2018-10-14 01:16:54 +0300

    Added usefull links for exersice 3

--- Exersice_3/solution.py
+++ Exersice_3/solution.py
@@ -1,3 +1,16 @@
+"""
+Usefull links:
+RE tutorial
+    https://www.guru99.com/python-regular-expressions-complete-tutorial.html
+RE online tester
+    https://regex101.com/
+RE groups: (), (?:), (?P<>)
+    https://stackoverflow.com/questions/18425386/re-findall-not-returning-full-match
+    https://stackoverflow.com/questions/10059673/named-regular-expression-group-pgroup-nameregexp-what-does-p-stand-for
+Memory efficient way to read file:
+    https://stackoverflow.com/questions/8009882/how-to-a-read-large-file-line-by-line-in-python
+    https://stackabuse.com/read-a-file-line-by-line-in-python/
+"""
 import re
 
 def logtolist(logfilename):
