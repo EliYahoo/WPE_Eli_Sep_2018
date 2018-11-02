f10c486c329e0f768915c6fd38b44df069ad7554
Eli <alexeykhitrov@gmail.com>
2018-11-02 20:33:35 +0200

    My solution for 6

--- Exercise_6/solution.py
+++ Exercise_6/solution.py
@@ -1,18 +1,32 @@
-from test_6 import all_people
+from collections import Counter
+from statistics import mean
+
+all_people = [{'name': 'Reuven', 'age': 48, 'hobbies': ['Python', 'cooking', 'reading']},
+              {'name': 'Atara', 'age': 17, 'hobbies': ['horses', 'cooking', 'art', 'reading']},
+              {'name': 'Shikma', 'age': 15, 'hobbies': ['Python', 'piano', 'cooking', 'reading']},
+              {'name': 'Amotz', 'age': 13, 'hobbies': ['biking', 'cooking']}]
 
 
+def average_age_under(people, maxage):
+    age_list = [person['age'] for person in people if person['age'] < maxage]
+    return mean(age_list) if age_list else 0
 
 
+def all_hobbies(people):
+    return set([hobby for person in people for hobby in person['hobbies']])
 
 
+def hobby_counter(people):
+    return Counter([hobby for person in people for hobby in person['hobbies']])
 
 
-
-
+def n_most_common(people, n_most_common_hobbies):
+    return [elem[0] for elem in
+            Counter([hobby for person in people for hobby in person['hobbies']]).most_common(n_most_common_hobbies)]
 
 
 def main():
-    cities_to_csv(gist_url, "test_output.csv")
+    average_age_under(all_people,-1)
 
 
 if __name__ == "__main__":
