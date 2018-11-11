2adabca03647cf12e750c2274562ed869206c96b
Eli <alexeykhitrov@gmail.com>
2018-11-12 01:02:02 +0200

    Ex_7

---
+++ Exercise_7/solution.py
@@ -0,0 +1,16 @@
+import os
+
+def func_to_dir(dir, func):
+    valid_results = {}
+    error_results = {}
+
+    for file_elem in os.listdir(dir):
+        try:
+            valid_results[file_elem] = func(file_elem)
+        except Exception as e:
+            error_results[file_elem] = e
+
+    return valid_results, error_results
+
+good, bad = func_to_dir("../Exercise_5",os.stat)
+print("The end")
