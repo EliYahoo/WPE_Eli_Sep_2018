8249980299730d436ef24c3d2c34741d39841faf
Eli <alexeykhitrov@gmail.com>
2018-11-20 23:27:50 +0200

    Solution 8

--- Exercise_8/solution.py
+++ Exercise_8/solution.py
@@ -1,9 +1,14 @@
+'''
+    Topics:
+        f-strings - https://realpython.com/python-f-strings/#old-school-string-formatting-in-python
+'''
 import random
+
 
 def create_math_problems(output):
     for x in range(100):
         print("{}{}{}{}".format(*[random.randint(-40,40) for x in range(4)]))
-
+        print(random.choice(['+','-']))
 
 def main():
     str = ""
